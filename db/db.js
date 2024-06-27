8b19f5698734a8cbd4228697ea15370091aba95a
om salunke <salunkeom474@gmail.com>
2024-06-27 09:16:20 -0700

    db link change and 10 user data added

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await connect('mongodb://localhost:27017/assignment_task');
+    await connect('mongodb+srv://smartcoder0852:zVWbXMnVgt7dnP0U@cluster0.lfofxmp.mongodb.net/backend_assignment');
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
